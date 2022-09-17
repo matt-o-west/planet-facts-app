7cee2e885fa57aa8c2f6521041e97dc4ed36f19a
matt-o-west <matt.omalley.west@gmail.com>
2022-09-16 23:00:09 -0400

    completed basic styling of nav bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 //import { useState } from 'react'
 import './App.css'
+import Nav from './components/Nav'
+import {BrowserRouter as Router} from "react-router-dom"
 
 function App() {
 
 
   return (
-    <div className="App">
-      
-    </div>
+  <Router>
+    <main className="App">
+      <Nav />
+    </main>
+  </Router>
   )
 }
 
