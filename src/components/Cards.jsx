30adcc1018a719470bfb56b4d60bf7e19c8cf88e
matt-o-west <matt.omalley.west@gmail.com>
2022-09-26 17:50:30 -0400

    completed basic styling for mobile

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,34 +1,31 @@
-
-
 //import {Link} from "react-router-dom"
-import "./Nav.css"
-import "./Cards.css"
+import './Nav.css'
+import './Cards.css'
 
 function Cards(props) {
-
   console.log(props.facts.rotation)
-  const temp = props.facts.temperature.match(/\d+/g).map(Number) + " °C"
+  const temp = props.facts.temperature.match(/\d+/g).map(Number) + ' °C'
 
-    return (
-      <div className="cards"> 
-        <div className="card">  
-            <div className="card-title">ROTATION TIME</div>
-            <div className="card-value">{props.facts.rotation}</div>
-        </div>  
-        <div className="card">  
-            <div className="card-title">REVOLUTION TIME</div>
-            <div className="card-value">{props.facts.revolution}</div>
-        </div>  
-        <div className="card">  
-            <div className="card-title">RADIUS</div>
-            <div className="card-value">{props.facts.radius}</div>
-        </div>  
-        <div className="card">  
-            <div className="card-title">AVERAGE TEMP.</div>
-            <div className="card-value">{temp}</div>
-        </div>  
+  return (
+    <div className='cards'>
+      <div className='card'>
+        <div className='card-title'>ROTATION TIME</div>
+        <div className='card-value'>{props.facts.rotation}</div>
       </div>
-    )
-  }
-  
-  export default Cards
+      <div className='card'>
+        <div className='card-title'>REVOLUTION TIME</div>
+        <div className='card-value'>{props.facts.revolution}</div>
+      </div>
+      <div className='card' id='radius'>
+        <div className='card-title'>RADIUS</div>
+        <div className='card-value'>{props.facts.radius}</div>
+      </div>
+      <div className='card' id='temp'>
+        <div className='card-title'>AVERAGE TEMP.</div>
+        <div className='card-value'>{temp}</div>
+      </div>
+    </div>
+  )
+}
+
+export default Cards
