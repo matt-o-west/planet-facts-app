68d87f2ccc3ee01ec2259ce7d387b560fa9279ce
matt-o-west <matt.omalley.west@gmail.com>
2022-09-26 19:32:28 -0400

    added mobile menu and initial styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,26 +1,26 @@
-import {Link, BrowserRouter} from "react-router-dom"
-import "./Nav.css"
+import { Link } from 'react-router-dom'
+import Menu from './Menu'
+import './Nav.css'
 
 function Nav() {
+  return (
+    <nav>
+      <span className='logo'>The Planets</span>
+      <Menu />
+      <div className='planet-links'>
+        <ul>
+          <Link to='/mercury'>Mercury</Link>
+          <Link to='/venus'>Venus</Link>
+          <Link to='/earth'>Earth</Link>
+          <Link to='/mars'>Mars</Link>
+          <Link to='/jupiter'>Jupiter</Link>
+          <Link to='/saturn'>Saturn</Link>
+          <Link to='/uranus'>Uranus</Link>
+          <Link to='/neptune'>Neptune</Link>
+        </ul>
+      </div>
+    </nav>
+  )
+}
 
-
-    return (
-      <nav>
-            <span className="logo">The Planets</span>
-            <div className="planet-links">
-            <ul>
-              <Link to="/mercury">Mercury</Link>
-              <Link to="/venus">Venus</Link>
-              <Link to="/earth">Earth</Link>
-              <Link to="/mars">Mars</Link>
-              <Link to="/jupiter">Jupiter</Link>
-              <Link to="/saturn">Saturn</Link>
-              <Link to="/uranus">Uranus</Link>
-              <Link to="/neptune">Neptune</Link>
-            </ul>
-            </div>
-      </nav>
-    )
-  }
-  
-  export default Nav
+export default Nav
